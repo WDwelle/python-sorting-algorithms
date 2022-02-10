1f6d7b69eaa4954bdcdc2ed1b880f3d5bfb9037f
willd <willdwelle@frontier.com>
2022-02-10 12:35:26 -0800

    added comments

--- bubble.py
+++ bubble.py
@@ -1,8 +1,11 @@
 def bubbleSort(arr):
+    #Loop through array
     for n in range(len(arr)-1, 0, -1):
         for i in range(n):
+            #compare values and sort
             if arr[i] > arr[i + 1]:
                 arr[i], arr[i + 1] = arr[i + 1], arr[i]
+#example input
 arr = [39,12,18,85,72,10,2,18]
 
 print("Unsorted list is,")
--- selectionSort.py
+++ selectionSort.py
@@ -1,12 +1,16 @@
 def selectionSort(arr):
+    #loop through array
     for i in range(len(arr)):
+        #set variable for our lowest value
         lowVal = i
         for j in range(i + 1, len(arr)):
+            #compare and switch values
             if arr[j] < arr[lowVal]:
                 lowVal = j
         arr[i], arr[lowVal] = arr[lowVal], arr[i]
 
+#example data set
+arr = [12, 8, 3, 20, 11]
 
-random_list_of_nums = [12, 8, 3, 20, 11]
-selectionSort(random_list_of_nums)
-print(random_list_of_nums)
+selectionSort(arr)
+print(arr)
