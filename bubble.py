0cbc1e2f05816012fdd39232ab754cf67f6f9582
WDwelle <willdwelle@frontier.com>
2022-02-18 16:01:52 -0800

    Main added bubble

--- bubble.py
+++ bubble.py
@@ -1,15 +1,23 @@
-def bubbleSort(arr):
-    #Loop through array
-    for n in range(len(arr)-1, 0, -1):
-        for i in range(n):
-            #compare values and sort
-            if arr[i] > arr[i + 1]:
-                arr[i], arr[i + 1] = arr[i + 1], arr[i]
-#example input
-arr = [39,12,18,85,72,10,2,18]
+    
+class BubbleSort:
 
-print("Unsorted list is,")
-print(arr)
-bubbleSort(arr)
-print("Sorted Array is, ")
-print(arr)
+    def bubbleSort(self, arr):
+        n = len(arr)
+
+        for i in range(n-1):
+
+            for j in range(0, n-i-1):
+
+                if arr[j] > arr[j + 1] :
+                    arr[j], arr[j + 1] = arr[j + 1], arr[j]
+
+    #example input
+    #arr = [39,12,18,85,72,10,2,18]
+
+    #starting data
+    #print("Unsorted list is,")
+    #print(arr)
+    #bubbleSort(arr)
+    #ending data
+    #print("Sorted Array is, ")
+    #print(arr)
--- main.py
+++ main.py
@@ -1,16 +1,12 @@
-# This is a sample Python script.
+from bubble import BubbleSort
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+arr = [10, 65, 12, 1, 7, 69, 85, 11, 15, 42]
 
+bubble = BubbleSort()
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+print("unsorted array:")
+print(arr)
 
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+print("sorted array:")
+bubble.bubbleSort(arr)
+print(arr)
