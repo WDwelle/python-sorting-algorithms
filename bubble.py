07592498b2b79a37c593014e7a83da6c8b4c2955
WDwelle <willdwelle@frontier.com>
2022-02-23 13:51:40 -0800

    changed comments

--- bubble.py
+++ bubble.py
@@ -1,4 +1,4 @@
-    
+
 class BubbleSort:
 
     def bubbleSort(self, arr):
@@ -10,14 +10,3 @@ class BubbleSort:
 
                 if arr[j] > arr[j + 1] :
                     arr[j], arr[j + 1] = arr[j + 1], arr[j]
-
-    #example input
-    #arr = [39,12,18,85,72,10,2,18]
-
-    #starting data
-    #print("Unsorted list is,")
-    #print(arr)
-    #bubbleSort(arr)
-    #ending data
-    #print("Sorted Array is, ")
-    #print(arr)
