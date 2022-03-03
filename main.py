a25dfe2f2f963b35670427760c996db51f6f92e0
WDwelle <willdwelle@frontier.com>
2022-03-03 13:28:50 -0800

    added css and flask server

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
+from flask import Flask, render_template
 from models.bubble import BubbleSort
 from models.selectionSort import SelectSort
+
+
+app = Flask(__name__)
+
+@app.route('/')
+
+def hello():
+    return render_template('index.html')
 
 arr = [10, 65, 12, 1, 7, 69, 85, 11, 15, 42]
 
@@ -7,21 +16,24 @@ bubble = BubbleSort()
 select = SelectSort()
 
 
-sort = input("which sort would you like to use (type: bubble or select)?")
+#sort = input("which sort would you like to use (type: bubble or select)?")
 
 
-if sort == ("bubble"):
-    print("unsorted array:")
-    print(arr)
+# if sort == ("bubble"):
+#     print("unsorted array:")
+#     print(arr)
 
-    print("sorted array:")
-    bubble.bubbleSort(arr)
-    print(arr)
+#     print("sorted array:")
+#     bubble.bubbleSort(arr)
+#     print(arr)
 
-elif sort == ("select"):
-    print("unsorted array:")
-    print(arr)
+# elif sort == ("select"):
+#     print("unsorted array:")
+#     print(arr)
 
-    print("sorted array:")
-    select.selectionSort(arr)
-    print(arr)
+#     print("sorted array:")
+#     select.selectionSort(arr)
+#     print(arr)
+
+
+app.run(debug=True)
